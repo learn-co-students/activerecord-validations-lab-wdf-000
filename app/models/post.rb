30077cadcc78468e407e86121ce5316c29861c4e
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-27 14:17:34 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w{ Fiction Non-Fiction } }
+  validate :is_clickbait?
+
+  def is_clickbait?
+    if title && !title.include?("You Won't Believe")
+      errors.add(:title)
+    end
+  end
 end

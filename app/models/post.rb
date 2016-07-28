3c4b23d0f87b1ae1f1252f4d16ce01a0242edf3d
blessedsoy <unmigong@gmail.com>
2016-07-28 10:50:25 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 100}
+  validates :summary, length: {maximum: 200}
+  validate :outside_the_choice
+  validate :not_clickbait
+
+  def outside_the_choice
+    if category != "Fiction" && category != "Non-Fiction"
+      errors.add(:category, "can't be outside the choices")
+    end
+  end
+
+  def not_clickbait
+    if title== "True Facts"
+      errors.add(:title, "No clickbait!!")
+    end
+  end
+
 end

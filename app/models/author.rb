687c63bb83f949b0e6a40f4a1e44c0391b7d2951
satub <satu.barnhill@gmail.com>
2016-08-30 13:35:03 -0400

    Done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :phone_number, numericality: true, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+  validate :clickbaity
+
+  def clickbaity
+    bait_word = ["Won't Believe", "Secret", "Top", "Guess"].find do |bait|
+      # binding.pry
+      title.match(bait) unless title.nil?
+    end
+
+    if !bait_word
+      errors.add(:title, "No clickbait-y enough of a title")
+    end
+
+  end
+
 end

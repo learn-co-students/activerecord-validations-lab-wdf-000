8c211a6e828a8e1c2a1ea667b17f383b077baa5a
Flatiron School <github email address>
2016-09-12 18:10:29 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,49 @@
 class Post < ActiveRecord::Base
+
+  include ActiveModel::Validations
+
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250}
+  validates :summary, length: { maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbaity
+
+
+
+  def clickbaity
+    bait_word = ["Won't Believe", "Secret", "Top", "Guess"].find do |bait|
+      title.match(bait) unless title.nil?
+    end
+
+    if !bait_word
+      errors.add(:title, "not clickbaity enough")
+    end
+  end
+
+
+  # validates_with MyValidator
+
+  # validates :title, inclusion: { in: %w(Secret Guess)}
+
+    # def clickbaity
+    #   if title.include?("Won't Believe")
+    #     # || !title.include?("Secret") || !title.include?("Top") || !title.include?("Guess")
+    #     errors.add(:title, "not clickbaity enough")
+    #   end
+    # end
+    #
+    #
+
 end
+
+
+#
+# class MyValidator < ActiveModel::Validator
+#
+#
+#   def validate(record)
+#     if !record.title.include?("Won't Believe") || !record.title.include?("Secret") || !record.title.include?("Top") || !record.title.include?("Guess")
+#       errors.add(:title, "not clickbaity enough")
+#     end
+#   end

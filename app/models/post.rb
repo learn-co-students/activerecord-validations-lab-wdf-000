a4ad7f26169e39a1040c8b8ee140249abbed0a06
Wendy Xu <guangw.xu@gmail.com>
2016-09-21 16:57:07 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,9 +8,28 @@ class Post < ActiveRecord::Base
   validate :title_is_clickbait
 
   def title_is_clickbait
-    unless title =~ /Won't Believe|Secret|Guess|Top\d+/
+    unless title =~ /Won't Believe|Secret|Guess|Top \d+/
       errors.add(:title, "This title is not clickbait")
     end
   end
 
+
+
+### what solution did:
+# validate :is_clickbait?
+
+#   CLICKBAIT_PATTERNS = [
+#     /Won't Believe/i,
+#     /Secret/i,
+#     /Top [0-9]*/i,
+#     /Guess/i
+#   ]
+
+#   def is_clickbait?
+#     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+#       errors.add(:title, "must be clickbait")
+#     end
+#   end
+
 end
+

aa848d6930c47e4146ef9721d7f6e5df6280b377
Bibi Khadiza <limakhadiza@yahoo.com>
2016-09-29 15:35:05 -0400

    done

--- Gemfile
+++ Gemfile
@@ -8,13 +8,12 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'pry'
 
 group :development, :test do
   gem 'rspec-rails'
-  gem 'pry'
 end
 
 group :development do
   gem 'web-console', '~> 2.0'
 end
-
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,35 @@
 class Post < ActiveRecord::Base
+
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction || Non-Fiction) }
+  validate :click_bait?
+
+  ARR = ["Won't Believe", "Secret", "Top", "Guess"]
+
+
+
+
+
+  def click_bait?
+    if title
+      if ARR.find do |word|
+        title.match(word)
+      end
+    else
+      errors.add(:title, "no clickbait")
+      end
+    end
+  end
+
+
+
+
+
+
+
+
+
 end
